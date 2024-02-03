0a16086507389645bd3408d727ea1cb4de8e1c70
tsano <69151078+tgtaku@users.noreply.github.com>
2024-02-03 14:07:53 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,11 @@
+// ファンクション定義
+const getSquaredValue = (num) =>{
+    const squaredValue = num * num;
+    return squaredValue;
+}
+
+// 引数の定義
+const num = 10;
+
+// 関数実行結果のログ出力
+console.log(getSquaredValue(num));
